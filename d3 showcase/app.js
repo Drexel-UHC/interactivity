1daa77e9d776480283850c37774dab058116cd65
Anuj <anujtanwar89@gmail.com>
2023-04-03 13:40:07 -0400

    d3 chart example

---
+++ d3 showcase/app.js
@@ -0,0 +1,50 @@
+const data = [
+    { country: "USA", value: 120 },
+    { country: "India", value: 150 },
+    { country: "Japan", value: 60 },
+    { country: "UK", value: 30 }
+];
+
+  
+// Create the SVG element
+    const svg = d3
+    .select(".chart")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 350);
+
+    // Define the scales
+    const xScale = d3
+    .scaleBand()
+    .range([0, 400])
+    .padding(0.1)
+    .domain(data.map(d => d.country));
+
+    const yScale = d3
+    .scaleLinear()
+    .range([250, 0])
+    .domain([0, d3.max(data, d => d.value)]);
+
+    // Create the bars
+    svg
+    .selectAll(".bar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", d => xScale(d.country))
+    .attr("y", d => yScale(d.value))
+    .attr("width", xScale.bandwidth())
+    .attr("height", d => 250 - yScale(d.value))
+    .text(d => d.value);
+
+    // Add the country labels
+    svg
+    .selectAll(".country-label")
+    .data(data)
+    .enter()
+    .append("text")
+    .attr("class", "country-label")
+    .attr("x", d => xScale(d.country) + xScale.bandwidth() / 2)
+    .attr("y", 265)
+    .text(d => d.country); // Show the name of the country
---
+++ obsidian/d3 notes/bar chart example d3 learnings.md
@@ -0,0 +1,9 @@
+
+## selectAll- to select all elemets 
+### select - to select one element in d3,
+so if we have on div we can call select for that div and have multiple items in that div we can do selectAll too select all elments
+
+#### when you use svg (which helps to manipulate data better ), use #attr instead of #style element, and in style we have to use value in a string and add + "px" but if we use #attr we can only write string and it reads as px without even specifying it
+
+## range 
+this is where we put smallest vlaue to show case chart lowest point and the highest value to show highest point [0, 100]
