48c0d507201c6a79c42c6497332593f997275f0b
Anuj <anujtanwar89@gmail.com>
2023-04-05 10:11:45 -0400

    charts test

---
+++ my-svelte-test/testApp/src/data/data.js
@@ -0,0 +1,18 @@
+export default [
+    { name: "John", age: 20, grade: 80, state: "New York"},
+    { name: "Jane", age: 21, grade: 90, state: "California"}, 
+    { name: "Bob", age: 22, grade: 70, state: "Texas"},
+    { name: "Alice", age: 23, grade: 60, state: "Florida"},
+    { name: "Joe", age: 24, grade: 50, state: "New York"},
+    { name: "Mary", age: 25, grade: 40, state: "California"},
+    { name: "Tom", age: 26, grade: 30, state: "Texas"},
+    { name: "Sue", age: 27, grade: 20, state: "Florida"},
+    { name: "Bill", age: 28, grade: 10, state: "New York"},
+    { name: "Kate", age: 29, grade: 0, state: "California"},
+    { name: "Sam", age: 30, grade: 100, state: "Texas"},
+    { name: "Ann", age: 31, grade: 95, state: "Florida"},
+    { name: "Alex", age: 32, grade: 85, state: "New York"},
+    { name: "Pete", age: 33, grade: 75, state: "California"},
+    { name: "Chris", age: 34, grade: 65, state: "Texas"},
+    { name: "Jill", age: 35, grade: 55, state: "Florida"}
+]
---
+++ my-svelte-test/testApp/src/routes/charts/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import data from "../../data/data.js";
+    console.log(data);
+    let height = 500;
+    let width = 500;
+
+    import { scaleLinear } from "d3-scale";
+    const xScale = scaleLinear()
+    .domain([0, 100])
+    .range([0, width]);
+
+    import { max } from "d3-array";
+    const yScale = scaleLinear()
+    .domain([0, 100])
+    .range([height, 0]);
+    
+
+</script>
+
+<svelte:head>
+	<title>My data charts</title>
+	<meta name="description" content="Chart page" />
+</svelte:head>
+
+<svg width={width} height={height}>
+    {#each data as student}
+        <circle cx={xScale(student.age)} cy={yScale(student.grade)} r="10" fill="red" stroke="black" /> 
+    {/each}
+</svg>
