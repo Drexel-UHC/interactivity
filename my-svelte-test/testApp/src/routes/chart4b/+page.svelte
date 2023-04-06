51921521fa07c3a3a017681389150d8af26b3741
Anuj <anujtanwar89@gmail.com>
2023-04-06 13:14:37 -0400

    Update +page.svelte

--- my-svelte-test/testApp/src/routes/chart4b/+page.svelte
+++ my-svelte-test/testApp/src/routes/chart4b/+page.svelte
@@ -105,7 +105,7 @@
 	      tooltip.style('display', 'none');
 	    })
 		.on('mousemove', function (event) {
-			const xPosition = event.pageX - 500;
+			const xPosition = event.pageX - 450;
 			const yPosition = event.pageY - 140;
 			tooltip.attr('transform', `translate(${xPosition}, ${yPosition})`);
 		});	      
