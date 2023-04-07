2ff30244914a6e2019ccd61207e8decb0ffaff7d
Anuj <anujtanwar89@gmail.com>
2023-04-07 16:47:33 -0400

    new test app without typeScript

---
+++ newtestapp/myapp/src/routes/+page.svelte
@@ -0,0 +1,86 @@
+<script>
+	import bar_image from '$lib/images/barchart1.png';
+	import pie_image from '$lib/images/piechart1.png';
+	import line_image from '$lib/images/linechart1.png';
+	import dot_image from '$lib/images/dotchart1.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="About test page" />
+</svelte:head>
+
+
+<div class="main-chart-box">
+	<div class="small-boxes">
+		<div class="inner-box">
+			<a href="../chart1">
+				<img class="mytest-image" src={bar_image} alt="chart 1" />
+				<h1 class="box-title">Bar Chart</h1>
+			</a>
+		</div>
+		<div class="inner-box">
+			<a href="../chart2">
+				<img class="mytest-image" src={pie_image} alt="chart 2" />
+				<h1 class="box-title">Pie Chart</h1>
+			</a>
+		</div>
+		<div class="inner-box">
+			<a href="../chart3">
+				<img class="mytest-image" src={line_image} alt="chart 3" />
+				<h1 class="box-title">Line chart</h1>
+			</a>
+		</div>
+		<div class="inner-box">
+			<a href="../chart3b">
+				<img class="mytest-image" src={line_image} alt="chart 3b" />
+				<h1 class="box-title">Line chart B</h1>
+			</a>
+		</div>
+		<div class="inner-box">
+			<a href="../chart4">
+				<img class="mytest-image" src={dot_image} alt="chart 4" />
+				<h1 class="box-title">Dot Chart</h1>
+			</a>
+		</div>
+		<div class="inner-box">
+			<a href="../chart4b">
+				<img class="mytest-image" src={dot_image} alt="chart 4b" />
+				<h1 class="box-title">Dot Chart B</h1>
+			</a>
+		</div>
+	</div>
+</div>
+
+<style>
+
+	a {
+		text-decoration: none;
+		color: #4d9aff;
+	}
+
+	.main-chart-box {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		border-radius: 30px;
+		background-color: #ff515139;
+	}
+	.small-boxes {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		align-items: center;
+	}
+	.inner-box{
+		border-radius: 30px;
+		background-color: #ffffff;
+		margin: 10px;
+	}
+	.mytest-image {
+		width: 100%;
+		max-width: 250px;
+		padding: 10px;
+	}
+</style>
---
+++ newtestapp/myapp/src/routes/chart1/+page.svelte
@@ -0,0 +1,69 @@
+<svelte:head>
+	<title>Bar Chart 1</title>
+	<meta name="description" content="About test page" />
+</svelte:head>
+
+<h1>D3 Bar Chart Example</h1>
+    <div class="chart"></div>
+<script>
+    import { onMount } from 'svelte';
+    import * as d3 from 'd3';
+    
+    const data = [
+      { country: "USA", value: 120 },
+      { country: "India", value: 150 },
+      { country: "Japan", value: 60 },
+      { country: "UK", value: 30 },
+      { country: "Brazil", value: 50},
+      { country: "France", value: 160}
+    ];
+  
+    let svg, xScale, yScale;
+  
+    onMount(() => {
+      // Create the SVG element
+      svg = d3.select('.chart')
+        .append('svg')
+        .attr('width', 500)
+        .attr('height', 350);
+  
+      // Define the scales
+      xScale = d3.scaleBand()
+        .range([0, 400])
+        .padding(0.1)
+        .domain(data.map(d => d.country));
+  
+      yScale = d3.scaleLinear()
+        .range([250, 0])
+        .domain([0, d3.max(data, d => d.value)]);
+  
+      // Create the bars
+      svg.selectAll('.bar')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', d => xScale(d.country))
+        .attr('y', d => yScale(d.value))
+        .attr('width', xScale.bandwidth())
+        .attr('height', d => 250 - yScale(d.value))
+        .text(d => d.value);
+  
+      // Add the country labels
+      svg.selectAll('.country-label')
+        .data(data)
+        .enter()
+        .append('text')
+        .attr('class', 'country-label')
+        .attr('x', d => xScale(d.country) + xScale.bandwidth() / 2)
+        .attr('y', 265)
+        .text(d => d.country);
+
+        svg.append('text')
+			.attr('x', 250)
+			.attr('y', 20)
+			.attr('text-anchor', 'middle')
+			.attr('font-size', '18px')
+			.text('Bar Chart');
+    });
+</script>
