6f4fd0f47b0f155f808d5c74e39a6be26947376b
Anuj <anujtanwar89@gmail.com>
2023-04-10 18:24:34 -0400

    :construction: :hammer: chart 3c done

--- my-svelte-test/testApp/src/routes/chart3c/+page.svelte
+++ my-svelte-test/testApp/src/routes/chart3c/+page.svelte
@@ -5,117 +5,156 @@
 
 <h1>D3 Line Chart Example</h1>
 <script>
-	import { onMount } from 'svelte';
-	import * as d3 from 'd3';
+    import { onMount } from 'svelte';
+    import * as d3 from 'd3';
+  
+    let data = [
+      { 
+        group: 'A', values: [
+            { x: 0, y: 20 }, 
+            { x: 1, y: 50 }, 
+            { x: 2, y: 30 }, 
+            { x: 3, y: 60 },
+            { x: 7, y: 76 },
+            { x: 5, y: 30 },
+        ] 
+    },
+      { 
+        group: 'B', values: [
+            { x: 0, y: 30 }, 
+            { x: 1, y: 40 }, 
+            { x: 2, y: 10 }, 
+            { x: 3, y: 50 },
+            { x: 7, y: 86 },
+            { x: 5, y: 90 },
+        ] 
+    },
+    ];
+  
+    let selectedGroup = 'A';
+  
+    let margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    let width = 600 - margin.left - margin.right;
+    let height = 400 - margin.top - margin.bottom;
+  
+    let x = d3.scaleLinear().range([0, width]);
+    let y = d3.scaleLinear().range([height, 0]);
+  
+    let xAxis = d3.axisBottom(x);
+    let yAxis = d3.axisLeft(y);
+  
+    let line = d3.line()
+      .x(d => x(d.x))
+      .y(d => y(d.y));
+  
+    let svg;
+    let xAxisEl;
+    let yAxisEl;
+    let lineGroup;
+    let circleGroup;
+  
+    onMount(() => {
+      svg = d3.select('#chart')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+  
+      xAxisEl = svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', `translate(0,${height})`);
+  
+      yAxisEl = svg.append('g')
+        .attr('class', 'y axis');
+  
+      lineGroup = svg.append('g')
+        .attr('class', 'line-group');
+  
+      circleGroup = svg.append('g')
+        .attr('class', 'circle-group');
+  
+      updateChart();
+    });
+  
+    function updateChart() {
+      let selectedData = data.find(d => d.group === selectedGroup);
+  
+      x.domain(d3.extent(selectedData.values, d => d.x));
+      y.domain([0, d3.max(selectedData.values, d => d.y)]);
+  
+      xAxisEl.transition()
+        .duration(500)
+        .call(xAxis);
+  
+      yAxisEl.transition()
+        .duration(500)
+        .call(yAxis);
+  
+      let lines = lineGroup.selectAll('.line')
+        .data([selectedData]);
+  
+      lines.enter().append('path')
+        .attr('class', 'line')
+        .attr('d', d => line(d.values))
+        .attr('fill', 'none')
+        .attr('stroke', 'black')
+        .merge(lines)
+        .transition()
+        .duration(500)
+        .attr('d', d => line(d.values));
+  
+      lines.exit()
+        .transition()
+        .duration(500)
+        .style('opacity', 0)
+        .remove();
+  
+      let circles = circleGroup.selectAll('.circle')
+        .data(selectedData.values);
+  
+      circles.enter().append('circle')
+        .attr('class', 'circle')
+        .attr('cx', d => x(d.x))
+        .attr('cy', d => y(d.y))
+        .attr('r', 0)
+        .attr('fill', 'black')
+        .merge(circles)
+        .transition()
+        .duration(500)
+        .attr('cx', d => x(d.x))
+        .attr('cy', d => y(d.y))
+        .attr('r', 6)
+        .attr('fill', 'purple');
 
-	const data = [
-		{ year: 2009, value: 64, group: '1' },
-		{ year: 2010, value: 30, group: '1' },
-		{ year: 2011, value: 55, group: '1' },
-		{ year: 2012, value: 50, group: '1' },
-		{ year: 2013, value: 60, group: '1' },
-		{ year: 2014, value: 35, group: '1' },
-		{ year: 2015, value: 70, group: '1' },
-		{ year: 2016, value: 50, group: '1' },
-		{ year: 2017, value: 60, group: '2' },
-		{ year: 2018, value: 50, group: '2' },
-		{ year: 2019, value: 60, group: '2' },
-		{ year: 2020, value: 120, group: '2' },
-		{ year: 2021, value: 130, group: '2' },
-		{ year: 2021, value: 60, group: '2' },
-		{ year: 2022, value: 127, group: '2' },
-		{ year: 2023, value: 160, group: '2' },
-	];
+        circles.exit()
+        .transition()
+        .duration(500)
+        .attr('r', 0)
+        .remove();
+        }
 
-	onMount(() => {
-		const svg = d3.select('#chart')
-			.attr('width', 500)
-			.attr('height', 300);
-
-		const xScale = d3.scaleLinear()
-			.domain(d3.extent(data, d => d.year))
-			.range([50, 450]);
-
-		const yScale = d3.scaleLinear()
-			.domain(d3.extent(data, d => d.value))
-			.range([250, 50]);
-
-		const line = d3.line()
-			.x(d => xScale(d.year))
-			.y(d => yScale(d.value))
-			.curve(d3.curveCardinal);
-
-		svg.append('path')
-			.datum(data)
-			.attr('d', line)
-			.attr('stroke', 'blue')
-			.attr('fill', 'none');
-		
-		const circles = svg.selectAll('circle')
-			.data(data)
-			.enter()
-			.append('circle')
-			.attr('cx', d => xScale(d.year))
-			.attr('cy', d => yScale(d.value))
-			.attr('r', 4)
-			.attr('fill', 'blue');
-
-		const xAxis = d3.axisBottom(xScale)
-			.ticks(5)
-			.tickFormat(d3.format('d'));
-
-		const yAxis = d3.axisLeft(yScale)
-			.ticks(5);
-
-		svg.append('g')
-			.attr('transform', 'translate(0, 250)')
-			.call(xAxis);
-
-		svg.append('g')
-			.attr('transform', 'translate(50, 0)')
-			.call(yAxis);
-		svg.append('text')
-			.attr('x', 250)
-			.attr('y', 290)
-			.attr('text-anchor', 'middle')
-			.text('Year');
-
-		svg.append('text')
-			.attr('transform', 'rotate(-90)')
-			.attr('x', -150)
-			.attr('y', 20)
-			.attr('text-anchor', 'middle')
-			.text('Value');
-			
-		const tooltip = d3.select('body')
-			.append('div')
-			.attr('class', 'tooltip')
-			.style('position', 'absolute')
-			.style('z-index', '10')
-			.style('visibility', 'hidden')
-			.style('background', 'white')
-			.style('padding', '5px')
-			.style('border', '1px solid black');
-
-		circles.on('mouseover', (event, d) => {
-			tooltip.style('visibility', 'visible')
-				.html(`<strong>Year:</strong> ${d.year}<br><strong>Value:</strong> ${d.value}`)
-				.style('left', `${event.pageX + 10}px`)
-				.style('top', `${event.pageY}px`);
-		}).on('mousemove', (event) => {
-			tooltip.style('left', `${event.pageX + 10}px`)
-				.style('top', `${event.pageY}px`);
-		}).on('mouseout', () => {
-			tooltip.style('visibility', 'hidden');
-		});
-
-		svg.append('text')
-			.attr('x', 250)
-			.attr('y', 20)
-			.attr('text-anchor', 'middle')
-			.attr('font-size', '18px')
-			.text('Curved Line Chart with tooltips');
-	});
+        function switchGroup() {
+        selectedGroup = selectedGroup === 'A' ? 'B' : 'A';
+        updateChart();
+        }
 </script>
+  
+  
+<div id="chart"></div>
+<div>
+	<button on:click={() => { selectedGroup = 'A'; updateChart(); }}>Group A</button>
+	<button on:click={() => { selectedGroup = 'B'; updateChart(); }}>Group B</button>
+</div>
 
-<svg id="chart"></svg>
+<style>
+	button {
+		margin: 10px;
+		padding: 10px;
+		font-size: 20px;
+		width: 100%;
+		max-width: 100px;
+	}
+</style>
+  
+  
