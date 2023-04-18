b9ed23558c844999543f3c07777ad0073eca3958
Anuj <anujtanwar89@gmail.com>
2023-04-18 17:27:33 -0400

    added stroke

--- apps/mainapp/src/routes/chartmapd/+page.svelte
+++ apps/mainapp/src/routes/chartmapd/+page.svelte
@@ -13,54 +13,63 @@
 	const height = 600;
   
 	function drawMap() {
-	  const svg = d3.select('#map')
-		.attr('width', width)
-		.attr('height', height);
+	  	const svg = d3.select('#map')
+			.attr('width', width)
+			.attr('height', height);
   
-	  const projection = d3.geoIdentity()
-		.fitSize([width, height], feature(topojsonData, topojsonData.objects.states));
-	  const path = d3.geoPath().projection(projection);
+		const projection = d3.geoIdentity()
+			.fitSize([width, height], feature(topojsonData, topojsonData.objects.states));
+			const path = d3.geoPath().projection(projection);
   
-	  svg.selectAll('path')
-		.data(feature(topojsonData, topojsonData.objects.states).features)
-		.enter().append('path')
-		.attr('d', path)
-		.attr('stroke', '#000')
-    	.attr('stroke-width', '3px')
-		.attr('fill', 'none');
+		svg.selectAll('path')
+			.data(feature(topojsonData, topojsonData.objects.states).features)
+			.enter().append('path')
+			.attr('d', path)
+			.attr('stroke', '#000')
+			.attr('stroke-width', '3px')
+			.attr('fill', 'none');
 
-    svg.selectAll('.county')
-		.data(feature(topojsonData, topojsonData.objects.counties).features)
-		.enter().append('path')
-		.attr('class', 'county')
-		.attr('d', path)
-		.attr('stroke', 'lightblue')
-		.attr('stroke-width', '1px')
-		.attr('fill', 'DarkSlateBlue')
-		.attr('fill-opacity', 0.5)
-		.on('mouseover', function(event, d) {
-			d3.select(this)
-          .transition()
-          .duration(20)
-          .attr('fill', 'black')
-          .attr('fill-opacity', 1);
-			
-			const tooltip = d3.select('#tooltip');
-			tooltip.html(`County: ${d.properties.name}<br>Data: ${Math.floor(Math.random() * 100)}`);
-			tooltip.style('display', 'block');
-			tooltip.style('left', `${event.pageX + 10}px`);
-			tooltip.style('top', `${event.pageY - 10}px`);
-			tooltip.style('font-size', '12px');
-			tooltip.style('background-color', 'Bisque');
-			tooltip.style('padding', '5px');
-			tooltip.style('border', '1px solid black');
-			tooltip.style('border-radius', '5px');
-			tooltip.style('position', 'absolute');
-			tooltip.style('z-index', '9999');
-			tooltip.style('opacity', '1');
-			tooltip.style('width', `${Math.sqrt(d.properties.value) * 5}px`);
-			tooltip.style('height', `${Math.sqrt(d.properties.value) * 5}px`);
-			})
+		svg.selectAll('.state')
+			.data(feature(topojsonData, topojsonData.objects.states).features)
+			.enter().append('path')
+			.attr('class', 'state') // Add class to state paths
+			.attr('d', path)
+			.attr('stroke', 'red')
+			.attr('stroke-width', '3px')
+			.attr('stroke-opacity', 1)
+			.attr('fill', 'none');
+
+		svg.selectAll('.county')
+			.data(feature(topojsonData, topojsonData.objects.counties).features)
+			.enter().append('path')
+			.attr('class', 'county')
+			.attr('d', path)
+			.attr('stroke', 'lightblue')
+			.attr('stroke-width', '1px')
+			.attr('fill', 'DarkSlateBlue')
+			.attr('fill-opacity', 0.5)
+			.on('mouseover', function(event, d) { d3.select(this)
+			.transition()
+			.duration(20)
+			.attr('fill', 'black')
+			.attr('fill-opacity', 1);
+				
+				const tooltip = d3.select('#tooltip');
+				tooltip.html(`County: ${d.properties.name}<br>Data: ${Math.floor(Math.random() * 100)}`);
+				tooltip.style('display', 'block');
+				tooltip.style('left', `${event.pageX + 10}px`);
+				tooltip.style('top', `${event.pageY - 10}px`);
+				tooltip.style('font-size', '12px');
+				tooltip.style('background-color', 'Bisque');
+				tooltip.style('padding', '5px');
+				tooltip.style('border', '1px solid black');
+				tooltip.style('border-radius', '5px');
+				tooltip.style('position', 'absolute');
+				tooltip.style('z-index', '9999');
+				tooltip.style('opacity', '1');
+				tooltip.style('width', `${Math.sqrt(d.properties.value) * 5}px`);
+				tooltip.style('height', `${Math.sqrt(d.properties.value) * 5}px`);
+				})
 		.on('mouseout', function(event, d) {
         d3.select(this)
           .transition()
