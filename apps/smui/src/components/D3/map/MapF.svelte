b8dda81da9713a5b6f070d6772fd8e4354ff84ab
Anuj <anujtanwar89@gmail.com>
2023-04-20 13:59:08 -0400

    Update MapF.svelte

--- apps/smui/src/components/D3/map/MapF.svelte
+++ apps/smui/src/components/D3/map/MapF.svelte
@@ -8,52 +8,65 @@
 	import * as d3 from 'd3';
 	import { feature } from 'topojson-client';
 	import topojsonData from '../../../data/us-states-new.json';
-  
+	
 	const width = 960;
 	const height = 600;
-	const legendWidth = 200;
-	const legendHeight = 20;
-	const legendMargin = 20;
 	
 	const counties = feature(topojsonData, topojsonData.objects.counties).features;
 	counties.forEach(d => {
-		d.properties.data = Math.floor(Math.random() * 1000);
+	  d.properties.data1 = Math.floor(Math.random() * 1000);
+	  d.properties.data2 = Math.floor(Math.random() * 1000);
 	});
-
+	
+	const dataOptions = [
+	  { value: 'data1', label: 'Data 1' },
+	  { value: 'data2', label: 'Data 2' },
+	];
+	
+	let selectedData = dataOptions[0].value;
+	
+	let svg;
+	let colorScale;
+	
+	function updateMap() {
+		colorScale = d3.scaleSequential()
+			.domain(d3.extent(counties, d => d.properties[selectedData]))
+			.interpolator(d3.interpolateBlues);
+	  
+		svg.selectAll('.county')
+			.transition()
+    		.duration(1000)
+			.attr('fill', d => colorScale(d.properties[selectedData]))
+			.attr('fill-opacity', 0.5);
+	}
+	
 	function drawMap() {
-	  const svg = d3.select('#map')
-		.attr('width', width)
-		.attr('height', height);
-  
-	  const projection = d3.geoIdentity()
-		.fitSize([width, height], feature(topojsonData, topojsonData.objects.states));
-	  const path = d3.geoPath().projection(projection);
-
-	  const colorScale = d3.scaleSequential()
-		.domain(d3.extent(counties, d => d.properties.data))
-		.interpolator(d3.interpolateBlues);
-  
-	  svg.selectAll('path')
-		.data(feature(topojsonData, topojsonData.objects.states).features)
-		.enter().append('path')
-		.attr('d', path)
-		// .attr('stroke', '#000')
-    	// .attr('stroke-width', '3px')
-		.attr('fill', 'none');
-
-    svg.selectAll('.county')
-		.data(counties)
-		.enter().append('path')
-		.attr('class', 'county')
-		.attr('d', path)
-		.attr('stroke', 'lightblue')
-		.attr('stroke-width', '1px')
-		.attr('fill', d => colorScale(d.properties.data))
-		.attr('fill-opacity', 0.5)
-		.on('mouseover', function(event, d) {
-			const tooltip = d3.select('#tooltip');
-			tooltip.html(`County: ${d.properties.name}<br>Data: ${d.properties.data}`);
-			tooltip.style('display', 'block');
+		svg = d3.select('#map')
+			.attr('width', width)
+			.attr('height', height);
+	  
+		const projection = d3.geoIdentity()
+			.fitSize([width, height], feature(topojsonData, topojsonData.objects.states));
+		const path = d3.geoPath().projection(projection);
+	
+		svg.selectAll('path')
+			.data(feature(topojsonData, topojsonData.objects.states).features)
+			.enter().append('path')
+			.attr('d', path)
+			.attr('fill', 'none');
+	
+		svg.selectAll('.county')
+			.data(counties)
+			.enter().append('path')
+			.attr('class', 'county')
+			.attr('d', path)
+			.attr('stroke', 'lightblue')
+			.attr('stroke-width', '1px')
+			.attr('fill-opacity', 0.5)
+			.on('mouseover', function(event, d) {
+		  	const tooltip = d3.select('#tooltip');
+		  	tooltip.html(`County: ${d.properties.name}<br>Data: ${d.properties[selectedData]}`);
+		 	tooltip.style('display', 'block');
 			tooltip.style('left', `${event.pageX + 10}px`);
 			tooltip.style('top', `${event.pageY - 10}px`);
 			tooltip.style('font-size', '12px');
@@ -66,13 +79,13 @@
 			tooltip.style('opacity', '1');
 			tooltip.style('width', `${Math.sqrt(d.properties.value) * 5}px`);
 			tooltip.style('height', `${Math.sqrt(d.properties.value) * 5}px`);
-			})
+		})
 		.on('mouseout', function(event, d) {
 			const tooltip = d3.select('#tooltip');
 			tooltip.style('display', 'none');
-        });
-
-		svg.selectAll('.state')
+		});
+	
+	  	svg.selectAll('.state')
 			.data(feature(topojsonData, topojsonData.objects.states).features)
 			.enter().append('path')
 			.attr('class', 'state')
@@ -82,7 +95,7 @@
 			.attr('stroke-opacity', 1)
 			.attr('fill', 'none');
 
-			svg.selectAll('.state-label')
+		svg.selectAll('.state-label')
 			.data(feature(topojsonData, topojsonData.objects.states).features)
 			.enter().append('text')
 			.attr('class', 'state-label')
@@ -93,60 +106,39 @@
 			.style('font-size', '12px')
 			.style('fill', 'black')
 			.style('font-weight', 'bold');
-
-		const legend = svg.append('g')
-			.attr('transform', `translate(${width - legendWidth - legendMargin}, ${legendMargin})`);
-
-		const legendScale = d3.scaleLinear()
-			.domain(d3.extent(counties, d => d.properties.data))
-			.range([0, legendWidth]);
-
-		const legendAxis = d3.axisBottom(legendScale)
-			.ticks(5)
-			.tickSize(legendHeight);
-
-		legend.append('g')
-			.call(legendAxis)
-			.attr('transform', `translate(0, ${legendHeight})`);
-
-		const gradient = legend.append('defs')
-			.append('linearGradient')
-			.attr('id', 'gradient')
-			.attr('x1', '0%')
-			.attr('y1', '0%')
-			.attr('x2', '100%')
-			.attr('y2', '0%');
-
-		gradient.append('stop')
-			.attr('offset', '0%')
-			.attr('stop-color', colorScale.range()[0]);
-
-		gradient.append('stop')
-			.attr('offset', '100%')
-			.attr('stop-color', colorScale.range()[1]);
-
-		legend.append('rect')
-			.attr('x', 0)
-			.attr('y', 0)
-			.attr('width', legendWidth)
-			.attr('height', legendHeight)
-			.attr('fill', 'url(#gradient)');
-
-		svg.append('text')
-			.attr('x', width - legendWidth - legendMargin - 70)
-			.attr('y', legendMargin + legendHeight + 5)
-			.text('Number of Data Points')
-			.style('text-anchor', 'middle')
-			.style('font-size', '12px')
-			.style('font-weight', 'bold');
-
-	}
   
-	onMount(() => {
-	  drawMap();
-	});
+  updateMap();
+  }
+  
+  onMount(() => {
+  drawMap();
+  });
 </script>
 
 <svg id="map"></svg>
+<div id="tooltip"></div>
 
-<div id="tooltip" style="display: none;"></div>
+<select id="data-select" bind:value={selectedData} on:change={updateMap}>
+	{#each dataOptions as option}
+		<option value={option.value} selected={selectedData === option.value}>{option.label}</option>
+	{/each}
+</select>  
+
+
+<style>
+	#tooltip {
+		display: none;
+	}
+	select {
+		margin: 10px;
+		padding: 10px;
+		font-size: 20px;
+		width: 100%;
+		max-width: 150px;
+	}
+	option {
+		font-size: 20px;
+		text-align: center;
+	}
+</style>
+  
