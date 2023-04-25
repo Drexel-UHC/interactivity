60024829be9ec5b398eb7e6e45df47f31e94102f
Anuj <anujtanwar89@gmail.com>
2023-04-25 17:02:34 -0400

    Update +page.svelte

--- apps/mainapp/src/routes/chartmaph/+page.svelte
+++ apps/mainapp/src/routes/chartmaph/+page.svelte
@@ -169,32 +169,6 @@
 			.attr('x', d => path.centroid(d)[0])
 			.attr('y', d => path.centroid(d)[1])
 			.text(d => d.properties.name);
-		
-		if (!zoomed) {
-			svg.transition()
-			.duration(750)
-			.call(projection.animate({
-				fit: feature(topojsonData, topojsonData.objects.states),
-				padding: 20
-			}));
-		} else {
-			const bounds = path.bounds(feature(topojsonData, topojsonData.objects.states).features.find(d => d.properties.name === selectedState));
-			const dx = bounds[1][0] - bounds[0][0];
-			const dy = bounds[1][1] - bounds[0][1];
-			const x = (bounds[0][0] + bounds[1][0]) / 2;
-			const y = (bounds[0][1] + bounds[1][1]) / 2;
-			const scale = 0.9 / Math.max(dx / width, dy / height);
-			const translate = [width / 2 - scale * x, height / 2 - scale * y];
-			svg.transition()
-				.duration(750)
-				.call(projection.animate({
-					translate,
-					scale
-			}).on('end', () => {
-				svg.selectAll('.county')
-				.attr('stroke', 'red');
-			}));
-		}
 	}
   
 	onMount(() => {
