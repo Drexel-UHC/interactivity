1bb5449ece7fc6f008e2bf45303067d365db88fc
Anuj <anujtanwar89@gmail.com>
2023-04-27 12:57:29 -0400

    testing :bug: fix

--- apps/mainapp/src/routes/chartmapi/+page.svelte
+++ apps/mainapp/src/routes/chartmapi/+page.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-	<title>Map E</title>
+	<title>Map EI</title>
 	<meta name="description" content="map page" />
 </svelte:head>
 
@@ -11,67 +11,46 @@
   
 	const width = 960;
 	const height = 600;
-	const legendWidth = 200;
-	const legendHeight = 20;
-	const legendMargin = 20;
-	
 	const counties = feature(topojsonData, topojsonData.objects.counties).features;
-	counties.forEach(d => {
-		d.properties.data = Math.floor(Math.random() * 1000);
-	});
 
-	function drawMap() {
-	  const svg = d3.select('#map')
-		.attr('width', width)
-		.attr('height', height);
+	let selectedState = 'All States';
+	let projection;
+
+	const zoomToState = (state) => {
+	  if (state === 'All States') {
+		projection = d3.geoIdentity().fitSize([width, height], feature(topojsonData, topojsonData.objects.states));
+	  } else {
+		const stateFeature = feature(topojsonData, topojsonData.objects.states).features.find(f => f.properties.name === state);
+		projection = d3.geoIdentity().fitSize([width, height], stateFeature);
+	  }
   
-	  const projection = d3.geoIdentity()
-		.fitSize([width, height], feature(topojsonData, topojsonData.objects.states));
 	  const path = d3.geoPath().projection(projection);
-
-	  const colorScale = d3.scaleSequential()
-		.domain(d3.extent(counties, d => d.properties.data))
-		.interpolator(d3.interpolateBlues);
   
-	  svg.selectAll('path')
-		.data(feature(topojsonData, topojsonData.objects.states).features)
-		.enter().append('path')
-		.attr('d', path)
-		// .attr('stroke', '#000')
-    	// .attr('stroke-width', '3px')
-		.attr('fill', 'none');
-
-    svg.selectAll('.county')
-		.data(counties)
-		.enter().append('path')
-		.attr('class', 'county')
-		.attr('d', path)
-		.attr('stroke', 'lightblue')
-		.attr('stroke-width', '1px')
-		.attr('fill', d => colorScale(d.properties.data))
-		.attr('fill-opacity', 0.5)
-		.on('mouseover', function(event, d) {
-			const tooltip = d3.select('#tooltip');
-			tooltip.html(`County: ${d.properties.name}<br>Data: ${d.properties.data}`);
-			tooltip.style('display', 'block');
-			tooltip.style('left', `${event.pageX + 10}px`);
-			tooltip.style('top', `${event.pageY - 10}px`);
-			tooltip.style('font-size', '12px');
-			tooltip.style('background-color', 'Bisque');
-			tooltip.style('padding', '5px');
-			tooltip.style('border', '1px solid black');
-			tooltip.style('border-radius', '5px');
-			tooltip.style('position', 'absolute');
-			tooltip.style('z-index', '9999');
-			tooltip.style('opacity', '1');
-			tooltip.style('width', `${Math.sqrt(d.properties.value) * 5}px`);
-			tooltip.style('height', `${Math.sqrt(d.properties.value) * 5}px`);
-			})
-		.on('mouseout', function(event, d) {
-			const tooltip = d3.select('#tooltip');
-			tooltip.style('display', 'none');
-        });
-
+	  d3.selectAll('.state')
+		.transition()
+		.duration(750)
+		.attr('d', path);
+	};
+  
+	function drawMap() {
+	  	const svg = d3.select('#map')
+			.attr('width', width)
+			.attr('height', height);
+	
+		const projection = d3.geoIdentity()
+			.fitSize([width, height], feature(topojsonData, topojsonData.objects.states));
+		const path = d3.geoPath().projection(projection);
+		// const colorScale = d3.scaleSequential()
+		// 	.domain(d3.extent(counties, d => d.properties.data))
+		// 	.interpolator(d3.interpolateBlues);
+		svg.selectAll('path')
+			.data(feature(topojsonData, topojsonData.objects.states).features)
+			.enter().append('path')
+			.attr('d', path)
+			.attr('fill', 'none');
+  
+	  	zoomToState(selectedState);
+  
 		svg.selectAll('.state')
 			.data(feature(topojsonData, topojsonData.objects.states).features)
 			.enter().append('path')
@@ -81,72 +60,38 @@
 			.attr('stroke-width', '2px')
 			.attr('stroke-opacity', 1)
 			.attr('fill', 'none');
-
-			svg.selectAll('.state-label')
-			.data(feature(topojsonData, topojsonData.objects.states).features)
-			.enter().append('text')
-			.attr('class', 'state-label')
-			.attr('x', d => path.centroid(d)[0])
-			.attr('y', d => path.centroid(d)[1])
-			.text(d => d.properties.name)
-			.style('text-anchor', 'middle')
-			.style('font-size', '12px')
-			.style('fill', 'black')
-			.style('font-weight', 'bold');
-
-		const legend = svg.append('g')
-			.attr('transform', `translate(${width - legendWidth - legendMargin}, ${legendMargin})`);
-
-		const legendScale = d3.scaleLinear()
-			.domain(d3.extent(counties, d => d.properties.data))
-			.range([0, legendWidth]);
-
-		const legendAxis = d3.axisBottom(legendScale)
-			.ticks(5)
-			.tickSize(legendHeight);
-
-		legend.append('g')
-			.call(legendAxis)
-			.attr('transform', `translate(0, ${legendHeight})`);
-
-		const gradient = legend.append('defs')
-			.append('linearGradient')
-			.attr('id', 'gradient')
-			.attr('x1', '0%')
-			.attr('y1', '0%')
-			.attr('x2', '100%')
-			.attr('y2', '0%');
-
-		gradient.append('stop')
-			.attr('offset', '0%')
-			.attr('stop-color', colorScale.range()[0]);
-
-		gradient.append('stop')
-			.attr('offset', '100%')
-			.attr('stop-color', colorScale.range()[1]);
-
-		legend.append('rect')
-			.attr('x', 0)
-			.attr('y', 0)
-			.attr('width', legendWidth)
-			.attr('height', legendHeight)
-			.attr('fill', 'url(#gradient)');
-
-		svg.append('text')
-			.attr('x', width - legendWidth - legendMargin - 70)
-			.attr('y', legendMargin + legendHeight + 5)
-			.text('Number of Data Points')
-			.style('text-anchor', 'middle')
-			.style('font-size', '12px')
-			.style('font-weight', 'bold');
-
 	}
-  
 	onMount(() => {
 	  drawMap();
 	});
+	$: {
+	  zoomToState(selectedState);
+	}
 </script>
 
 <svg id="map"></svg>
-
-<div id="tooltip" style="display: none;"></div>
+<select bind:value={selectedState}>
+	<option value="All States">All States</option>
+	<option value="Texas">Texas</option>
+	<option value="California">California</option>
+	<option value="Utah">Utah</option>
+	<option value="New York">New York</option>
+	<option value="Florida">Florida</option>
+	<option value="Illinois">Illinois</option>
+</select>
+<style>
+	select {
+		margin: 10px;
+		padding: 10px;
+		font-size: 20px;
+		width: 100%;
+		max-width: 200px;
+		border-radius: 10px;
+		border: 2px solid DeepSkyBlue;
+		background-color: lightblue;
+	}
+	option {
+		font-size: 20px;
+		text-align: center;
+	}
+</style>
