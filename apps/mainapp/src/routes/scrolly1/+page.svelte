f8cfa3fdc00f57078d96b309c74d38904efea4de
Anuj <anujtanwar89@gmail.com>
2023-05-08 16:40:42 -0400

    three js testing

--- apps/mainapp/src/routes/scrolly1/+page.svelte
+++ apps/mainapp/src/routes/scrolly1/+page.svelte
@@ -1,1 +1,13 @@
 <!-- gsap not working -->
+
+<!-- src/App.svelte -->
+<script>
+    import Thing from '../../newcomp/Thing.svelte';
+  </script>
+  
+  <svelte:head>
+    <title>My Three Thing</title>
+  </svelte:head>
+  
+  <Thing />
+  
