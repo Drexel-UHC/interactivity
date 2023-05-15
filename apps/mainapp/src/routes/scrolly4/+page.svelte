2daeeccb4782eec6e67df8551a3c17ec8834de7a
Anuj <anujtanwar89@gmail.com>
2023-05-15 13:46:09 -0400

    Update +page.svelte

--- apps/mainapp/src/routes/scrolly4/+page.svelte
+++ apps/mainapp/src/routes/scrolly4/+page.svelte
@@ -168,10 +168,10 @@
 			.remove();
 	}
   
-	function switchGroup() {
-		selectedGroup = selectedGroup === 'A' ? 'B' : 'A';
-		updateChart();
-	}
+	// function switchGroup() {
+	// 	selectedGroup = selectedGroup === 'A' ? 'B' : 'A';
+	// 	updateChart();
+	// }
 </script>
   
 <style>
