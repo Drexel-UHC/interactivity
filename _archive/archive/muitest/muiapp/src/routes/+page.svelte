e40af10464caa50e9f0959246e48e1628d0d9619
rl627 <rl627@drexel.edu>
2023-05-22 10:27:37 -0400

    moved _archive folder to anuj repo

--- _archive/archive/muitest/muiapp/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Button from '@smui/button'
-</script>
-
-<h1>Hello World</h1>
-
-
-<Button on:click={()=> alert('it worked')}>Click Me</Button>
-<Button on:click={()=> alert('it worked')} color="secondary">Click Me</Button>
