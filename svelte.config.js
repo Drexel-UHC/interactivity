ecad5488bcba45bb3ba626eca4e84065e389a3ba
rl627 <rl627@drexel.edu>
2023-05-22 13:16:38 -0400

    revert to SSG #33

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
 		adapter: adapter({
-			// see the 'Deployment configuration' section below
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
 		})
 	}
 };
