4ebbe6669162fd33e681431325045b126ac726ec
rl627 <rl627@drexel.edu>
2023-05-22 14:07:18 -0400

    refactor scrolly catalog script: removed unsued code + added chips

--- src/routes/scrollies/+page.svelte
+++ src/routes/scrollies/+page.svelte
@@ -1,94 +1,136 @@
 <script>
 	import data from '../../lib/catalog/scrollies.json';
 	import Item from '../../lib/catalog/Item.svelte';
-	import { ListBox, ListBoxItem, InputChip } from '@skeletonlabs/skeleton';
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
 
 	// Tags
 	let tags = [...new Set(data.flatMap((item) => item.tech))];
-	let selectedTag = 999;
-	let selectedTagName = tags[selectedTag];
-	$: selectedTagName = tags[selectedTag];
 
 	// ListBox
-	let valueMultiple = [];
+	let selectedTags = [];
 
 	// Text Search
 	let search = '';
 
 	// Visible scrollies
 	$: visibleScrollies = data.filter((item) => {
-		if ((valueMultiple.length === 0) & (search === '')) return true;
-		else if ((valueMultiple.length === 0) & (search !== ''))
+		if ((selectedTags.length === 0) & (search === '')) return true;
+		else if ((selectedTags.length === 0) & (search !== ''))
 			return item.name.toLowerCase().includes(search.toLowerCase());
-		else if ((valueMultiple.length !== 0) & (search === '')) {
-			return valueMultiple.some((tag) => item.tech.includes(tag));
+		else if ((selectedTags.length !== 0) & (search === '')) {
+			return selectedTags.some((tag) => item.tech.includes(tag));
 		} else {
 			return (
-				valueMultiple.some((tag) => item.tech.includes(tag)) &&
+				selectedTags.some((tag) => item.tech.includes(tag)) &&
 				item.name.toLowerCase().includes(search.toLowerCase())
 			);
 		}
 	});
 
-	function handleRemoveFilter() {
-		selectedTag = 999;
-	}
+	const handleTagRemove = (tag) => {
+		selectedTags = selectedTags.filter((t) => t !== tag);
+	};
 </script>
 
-<div class="container topRow">
-	<input
-		class="input"
-		bind:value={search}
-		title="Input (text)"
-		type="text"
-		placeholder="search ... "
-	/>
-</div>
-
-<div class="container">
-	<div class="sidebar">
-		<div class="card">
-			<header class="card-header text-center text-2xl font-bold">Categories</header>
-			<section class="p-4">
-				<ListBox multiple>
-					{#each tags as tag, i}
-						<ListBoxItem bind:group={valueMultiple} name="medium" value={tag}>{tag}</ListBoxItem>
-					{/each}
-				</ListBox>
-			</section>
+<div class="global">
+	<div class="topRow">
+		<input
+			class="input"
+			bind:value={search}
+			title="Input (text)"
+			type="text"
+			placeholder="search ... "
+		/>
+		<div class="chipRow">
+			{#if selectedTags.length > 0}
+				{#each selectedTags as tag}
+					<span
+						class="chip variant-soft"
+						on:click={() => {
+							handleTagRemove(tag);
+						}}
+						on:keypress
+					>
+						<span>{tag}</span>
+						<span>|</span>
+						<span>x</span>
+					</span>
+				{/each}
+			{/if}
 		</div>
 	</div>
 
-	<div class="content">
-		<div style="padding-bottom: 20px;" />
-		{#each visibleScrollies as item (item.id)}
-			<Item {...item} />
-		{/each}
+	<div class="catalog-container">
+		<div class="catalog">
+			<div class="sidebar">
+				<div class="card">
+					<header class="card-header text-center text-2xl font-bold">Categories</header>
+					<section class="p-4">
+						<ListBox multiple>
+							{#each tags as tag, i}
+								<ListBoxItem bind:group={selectedTags} name="medium" value={tag}>{tag}</ListBoxItem>
+							{/each}
+						</ListBox>
+					</section>
+				</div>
+			</div>
+
+			<div class="content">
+				<div style="padding-bottom: 20px;" />
+				{#each visibleScrollies as item (item.id)}
+					<Item {...item} />
+				{/each}
+			</div>
+		</div>
 	</div>
 </div>
 
 <style>
+	.global {
+		display: flex;
+		flex-direction: column;
+		padding-left: 5%;
+		padding-right: 5%;
+	}
+
 	.topRow {
 		padding: 1rem;
 		flex-direction: column;
+		margin-bottom: 2rem;
+		padding-left: 10%;
+		padding-right: 10%;
+	}
+	.chipRow {
+		display: flex;
+		flex-direction: row;
+		margin-top: 5px;
+	}
+	.chip {
+		width: min-content;
+		margin-left: 10px;
 	}
 
-	.container {
+	.catalog-container {
+		position: fixed;
+		width: 80%;
+		margin-top: 6rem;
+	}
+
+	.catalog {
 		display: flex;
 		padding-left: 10%;
 		padding-right: 10%;
 	}
+
 	.sidebar {
 		width: 200px;
-
-		right: 0;
 		height: 100%;
-
 		padding: 10px;
-		box-sizing: border-box;
+		margin: 0px;
 	}
 	.content {
 		width: calc(100% - 200px);
+		margin: 0px;
 		padding: 10px;
 	}
 </style>
